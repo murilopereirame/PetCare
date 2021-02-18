81f2e00e42035edcdad5f780abc6a580a7601ade
Murilo Pereira <mrex@tuta.io>
2021-02-18 19:32:11 -0300

    feat: Adicionado rota para pets curtido

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -156,7 +156,7 @@ export default class UserController {
 
   likedPets = async (request: Request, response: Response) => {
     Database.getInstance().getConnection().then(conn => {
-      conn.manager.findOne(User, request.params.id).then((rt) => {        
+      conn.manager.findOne(User, request.params.id, {relations: ["pets", "likedPets"]}).then((rt) => {        
         return response.status(200).json({
           rt
         })
